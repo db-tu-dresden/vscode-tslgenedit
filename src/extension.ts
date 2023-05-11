7ee02da36734bf8365d3488065b7b91e6d83a42d
Alexander Krause <Alexander.Krause@tu-dresden.de>
2023-05-11 08:13:14 +0200

    dummy code for running tslgen as a daemon to speedup the generation process

--- src/extension.ts
+++ src/extension.ts
@@ -5,11 +5,15 @@ import { tslEditorExtension } from './editor/editor_extension';
 import { TSLEditorAutoComplete } from './editor/autocomplete';
 import { YAMLProcessingMode } from './editor/enums';
 import * as fs from 'fs';
+import { spawn } from 'node:child_process';
+import { ChildProcess } from 'child_process';
+import { FileSystemUtils } from './utils/files';
 
 // let tslPreviewPanel: vscode.WebviewPanel | undefined;
 
 let tslTerminal: vscode.Terminal | undefined;
 
+let generator_child: ChildProcess | undefined;
 
 export async function activate(context: vscode.ExtensionContext) {
     tslEditorExtension.update();
@@ -65,7 +69,7 @@ export async function activate(context: vscode.ExtensionContext) {
         const _data = await tslEditorExtension.renderCurrentSelection(YAMLProcessingMode.BuildRunAndTest) as TSLEditorPreview.PreviewMetaData;
         if (_data.buildable) {
             const tslPwd = TSLGeneratorModel.getTSLRootFolderForCurrentActiveFile();
-            const tslTempBuildDir = `tslgenEdit-build`;
+            const tslTempBuildDir = `.tslgenEdit-build`;
             const tslTempDir = `${tslPwd?.fsPath}/${tslTempBuildDir}`;
             if (!tslTerminal) {
                 tslTerminal = vscode.window.createTerminal(`TSL Terminal`);
@@ -85,6 +89,56 @@ export async function activate(context: vscode.ExtensionContext) {
             vscode.window.showErrorMessage(`No buildable primitive selected.`);
         }
     }));
+
+
+    let idx = 0;
+    let generator_ready = false;
+    context.subscriptions.push(vscode.commands.registerCommand('tslgen-edit.send_text_to_dummy', async () => {
+        console.log("Triggered child process");
+        if (generator_ready) {
+            console.log("Gen is ready.");
+        }
+        if (!generator_child) {
+            const tslPwd = TSLGeneratorModel.getTSLRootFolderForCurrentActiveFile()?.fsPath;
+            if (tslPwd) {
+                let pyPath = FileSystemUtils.joinPath(tslPwd, "main.py");
+
+                console.log(`Spawning for: ${pyPath}, outDir is: ${tslPwd}/.tslgenEdit-build`);
+                generator_child = spawn(`python3`, [`${pyPath}`, "-o", `${tslPwd}/.tslgenEdit-build`,"-d","-s"]);
+                console.log(`Spawn command: python3 ${[`${pyPath}`, "-o", `${tslPwd}/.tslgenEdit-build`,"-d","-s"].join(" ")}`)
+                generator_child.stdout?.on('data', (data) => {
+                    console.log(`stdout: ${data}`);
+                    if ( data.includes( "Ready" ) || data.includes( "Done" ) ) {
+                        console.log("Generator is ready again");
+                        generator_ready = true;
+                    }
+                });
+
+                generator_child.stderr?.on('data', (data) => {
+                    console.error(`stderr: ${data}`);
+                });
+
+                generator_child.on('close', (code) => {
+                    console.log(`child process exited with code ${code}`);
+                });
+
+            }
+        } else {
+            if ( generator_ready ) {
+                console.log(`Writing to stdin of generator: {"lscpu_flags":["sse","sse2","sse3","sse4_1","sse4_2","avx","avx2"], "primitives": ["load"]}`);
+                generator_ready = false;
+                generator_child.stdin?.write(`{"lscpu_flags":["sse","sse2","sse3","sse4_1","sse4_2","avx","avx2"], "primitives": ["load"]}\n`);
+            }
+        }
+        idx += 1
+    }));
+
+    context.subscriptions.push(vscode.commands.registerCommand('tslgen-edit.close_dummy', async () => {
+        console.log("Closing child process...");
+        generator_child?.kill("SIGINT");
+        generator_child = undefined;
+    }));
+
 
     vscode.window.onDidCloseTerminal((terminal) => {
         if (terminal.name === TSL_TERMINAL_IDENT) {
